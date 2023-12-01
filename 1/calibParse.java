7593244d7ecfc96ea8439ca9d9e58d37e59a8bb8
Niqqaficent <basgug25@gmail.com>
2023-12-01 14:03:54 +0100

    Getting started

---
+++ 1/calibParse.java
@@ -0,0 +1,19 @@
+/**
+ * A simple program that offers a solution to 
+ * the first problem of advent of code.
+ *
+ * Takes a stream of inputs.
+ * Parses the data to extract a two digit number.
+ * Sums up all the digits.
+ *
+ * @author Basliel B. Gugsa
+ * @version 01/12/23
+ */
+
+import java.util.Scanner;
+
+public class calibParse {
+
+    public static void main(String args) {}
+
+}
